32c82d1873d4030efbf186659f50c7d63585024c
Alameddin Çelik <alameddinc@gmail.com>
2022-10-15 19:52:41 +0300

    Polat örneği yenilendi

--- main.go
+++ main.go
@@ -1,43 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"github.com/go-turk/adana/user"
-)
+import "fmt"
+
+type Kisi struct {
+	Ad    string
+	Soyad string
+	Yas   int
+	Anne  *Kisi
+}
+
+func NewKisi(ad string, soyad string, yas int) *Kisi {
+	return &Kisi{
+		Ad:    ad,
+		Soyad: soyad,
+		Yas:   yas,
+	}
+}
 
 func main() {
-	polatAlemdar := user.NewKisi("Polat", "Alemdar", 55)
-	// Polatın babası
-	omerCandan := user.NewKisi("Ömer", "Candan", 76)
-	// Polatın annesi
-	nazifeCandan := user.NewKisi("Nazife", "Candan", 72)
-	// Polatın Baba Dedesi
-	ahmetCandan := user.NewKisi("Ahmet", "Candan", 98)
-	// Polatın Babaannesi
-	ayseCandan := user.NewKisi("Ayşe", "Candan", 96)
-	// Polatın Anne Dedesi
-	necipAksakalli := user.NewKisi("Necip", "Aksakallı", 98)
-	// Polatın Anneannesi
-	necibeAksakalli := user.NewKisi("Necibe", "Aksakallı", 96)
-	// tanımalamalrı yapalım
-	polatAlemdar.SetBaba(omerCandan)
-	polatAlemdar.SetAnne(nazifeCandan)
-	omerCandan.SetBaba(ahmetCandan)
-	omerCandan.SetAnne(ayseCandan)
-	nazifeCandan.SetBaba(necipAksakalli)
-	nazifeCandan.SetAnne(necibeAksakalli)
+	polatAlemdar := NewKisi("Polat", "Alemdar", 30)
+	nazifeHanim := NewKisi("Nazife", "Candan", 76)
+	polatAlemdar.Anne = nazifeHanim
 
-	// istersek şöyle de tanımlayabilirdir
-	polatAlemdar.Baba.Baba = ahmetCandan
-	polatAlemdar.Baba.Anne = ayseCandan
-	polatAlemdar.Anne.Baba = necipAksakalli
-	polatAlemdar.Anne.Anne = necibeAksakalli
-
-	fmt.Println(polatAlemdar)
-	fmt.Println(polatAlemdar.Baba)
-	fmt.Println(polatAlemdar.Baba.Baba)
-	fmt.Println(polatAlemdar.Baba.Anne)
-	fmt.Println(polatAlemdar.Anne)
-	fmt.Println(polatAlemdar.Anne.Baba)
+	fmt.Println(polatAlemdar.Ad)
+	fmt.Println(polatAlemdar.Anne.Ad)
 	fmt.Println(polatAlemdar.Anne.Anne)
+
 }
